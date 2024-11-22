8ede11876dd3ebd8743c0b62a03441b249546ae5
Khuat Quang Hung <138696615+hygef-v4@users.noreply.github.com>
2024-11-23 01:13:54 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'hygef-v4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Responsive Web Design NoteBook',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'hygef-v4',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Note tổng hợp kiến thức Wed201c <3',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
